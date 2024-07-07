887b4b991ac9825ddb924a1e5c57554e15347fcf
schlouff <33584736+schlouff@users.noreply.github.com>
2024-07-07 18:57:55 +0200

    korrigiert

--- meme_creator.py
+++ meme_creator.py
@@ -7,33 +7,40 @@ from io import BytesIO
 def main():
     st.title('Meme Creator')
 
-    if st.button('MemePDF erstellen'):
-        # PDF erstellen
-        pdf = create_10x15_meme_pdf()
+    # Eingabefeld für den Memetext
+    st.subheader("Schreibe hier deinen Wenn-du-Satz auf")
+    meme_text = st.text_input("Wenn-du-Satz", "")
 
-        # Generiere einen Timestamp für den Dateinamen
-        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    if st.button('Memetext hinzufügen'):
+        if meme_text:
+            # PDF erstellen
+            pdf = create_10x15_meme_pdf(meme_text=meme_text)
 
-        # Dateiname für GCS
-        gcs_filename = f"meme_pdf_{timestamp}.pdf"
+            # Generiere einen Timestamp für den Dateinamen
+            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 
-        # Bucket-Name
-        bucket_name = "vse-schamstaton24-07"
+            # Dateiname für GCS
+            gcs_filename = f"meme_pdf_{timestamp}.pdf"
 
-        try:
-            # Upload zur Google Cloud Storage
-            upload_pdf_to_gcs(bucket_name, pdf, f"MemePDFs/{gcs_filename}")
-            st.success(f"PDF erfolgreich in Google Cloud Storage hochgeladen: {gcs_filename}")
-        except Exception as e:
-            st.error(f"Fehler beim Hochladen in Google Cloud Storage: {str(e)}")
+            # Bucket-Name
+            bucket_name = "vse-schamstaton24-07"
 
-        # Download-Button für den Benutzer
-        st.download_button(
-            label=f"Meme PDF herunterladen ({timestamp})",
-            data=pdf,
-            file_name=gcs_filename,
-            mime="application/pdf"
-        )
+            try:
+                # Upload zur Google Cloud Storage
+                upload_pdf_to_gcs(bucket_name, pdf, f"MemePDFs/{gcs_filename}")
+                st.success(f"PDF erfolgreich in Google Cloud Storage hochgeladen: {gcs_filename}")
+            except Exception as e:
+                st.error(f"Fehler beim Hochladen in Google Cloud Storage: {str(e)}")
+
+            # Download-Button für den Benutzer
+            st.download_button(
+                label=f"Meme PDF herunterladen ({timestamp})",
+                data=pdf,
+                file_name=gcs_filename,
+                mime="application/pdf"
+            )
+        else:
+            st.warning("Bitte gib einen Wenn-du-Satz ein.")
 
 if __name__ == '__main__':
     main()
