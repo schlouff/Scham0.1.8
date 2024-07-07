834bc276ed924745ec178f57e5d173343b413849
schlouff <33584736+schlouff@users.noreply.github.com>
2024-07-07 21:58:11 +0200

    Füge mehrzeilige Meme-Text-Unterstützung hinzu
    - Ändere create_10x15_meme_pdf in memepdf_utils.py
    - Implementiere Text-Wrapping für bis zu 4 Zeilen
    - Zentriere den Meme-Text unter dem Bild
    - Füge Ellipsis (...) für zu lange Texte hinzu
    
    Diese Änderung verbessert die Lesbarkeit von längeren Meme-Texten,
    indem sie einen Zeilenumbruch ermöglicht und den Text besser im
    verfügbaren Raum positioniert.

--- memepdf_utils.py
+++ memepdf_utils.py
@@ -19,8 +19,6 @@ def create_10x15_meme_pdf(user_name="PlaceholderName", meme_text="", image_url=N
     page_height = 15 * cm
     c = canvas.Canvas(buffer, pagesize=(page_width, page_height), pdfVersion=(1, 5))
 
-    # Metadaten und andere Einstellungen...
-
     margin = 0.5 * cm
     max_width = page_width - 2 * margin
     max_height = page_height * 0.7
@@ -34,10 +32,8 @@ def create_10x15_meme_pdf(user_name="PlaceholderName", meme_text="", image_url=N
             if img.mode == 'CMYK':
                 img = img.convert('RGB')
 
-            # Seitenverhältnis des Bildes berechnen
             img_ratio = img.width / img.height
 
-            # Bildgröße unter Beibehaltung des Seitenverhältnisses berechnen
             if max_width / max_height > img_ratio:
                 image_height = max_height
                 image_width = image_height * img_ratio
@@ -45,11 +41,9 @@ def create_10x15_meme_pdf(user_name="PlaceholderName", meme_text="", image_url=N
                 image_width = max_width
                 image_height = image_width / img_ratio
 
-            # Bildposition berechnen
             x = (page_width - image_width) / 2
             y = page_height - image_height - margin
 
-            # Bild in PDF einfügen
             img_reader = ImageReader(img)
             c.drawImage(img_reader, x, y, width=image_width, height=image_height)
         except Exception as e:
@@ -57,11 +51,37 @@ def create_10x15_meme_pdf(user_name="PlaceholderName", meme_text="", image_url=N
             img = create_placeholder_image(int(max_width), int(max_height))
             c.drawImage(ImageReader(img), margin, y, width=max_width, height=max_height)
 
-    # Memetext hinzufügen
+    # Memetext hinzufügen (mehrzeilig)
     c.setFillColorRGB(0, 0, 0)  # Schwarz
+    pdfmetrics.registerFont(TTFont('Helvetica', 'Helvetica.ttf'))
     c.setFont("Helvetica", 14)
-    text_width = c.stringWidth(meme_text, "Helvetica", 14)
-    c.drawString((page_width - text_width) / 2, y - 1 * cm, meme_text)
+
+    # Text in Zeilen aufteilen
+    words = meme_text.split()
+    lines = []
+    current_line = []
+    for word in words:
+        test_line = ' '.join(current_line + [word])
+        if c.stringWidth(test_line, "Helvetica", 14) <= max_width:
+            current_line.append(word)
+        else:
+            lines.append(' '.join(current_line))
+            current_line = [word]
+    if current_line:
+        lines.append(' '.join(current_line))
+
+    # Maximale Anzahl von Zeilen begrenzen
+    max_lines = 4
+    if len(lines) > max_lines:
+        lines = lines[:max_lines - 1] + ['...']
+
+    # Text zentriert zeichnen
+    text_height = len(lines) * 20  # Zeilenhöhe
+    start_y = y - 1 * cm - text_height
+    for i, line in enumerate(lines):
+        text_width = c.stringWidth(line, "Helvetica", 14)
+        x = (page_width - text_width) / 2
+        c.drawString(x, start_y + i * 20, line)
 
     # Namen hinzufügen
     c.setFillColorRGB(0.5, 0.5, 0.5)  # Grau
