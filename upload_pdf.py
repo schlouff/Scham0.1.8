089f4ae9fb355f8a170ebb361a0132109a30f0f8
schlouff <33584736+schlouff@users.noreply.github.com>
2025-06-14 20:36:24 +0200

    ersetze gcs_config copy durch Dictionary

--- upload_pdf.py
+++ upload_pdf.py
@@ -4,10 +4,10 @@ from google.oauth2 import service_account
 import streamlit as st
 
 def upload_pdf_to_gcs(bucket_name, source_file, destination_blob_name):
-    gcs_config =         gcs_config = dict(st.secrets["gcs_bucket"])
+    gcs_config = dict(st.secrets["gcs_bucket"])
     gcs_config["private_key"] = gcs_config["private_key"].replace('\\n', '\n')
 
-    credentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"])
+    credentials = service_account.Credentials.from_service_account_info(st.secrets["gcs_bucket"])
     storage_client = storage.Client(credentials=credentials)
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_blob_name)
