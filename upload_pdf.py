31ba844ccc7a0ae5f6d174a459df8f177e242682
schlouff <33584736+schlouff@users.noreply.github.com>
2025-06-24 14:14:05 +0200

    Update upload_pdf.py
    set     storage_client = storage.Client(credentials=credentials)

--- upload_pdf.py
+++ upload_pdf.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 def upload_pdf_to_gcs(bucket_name, source_file, destination_blob_name):
     credentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"])
-    storage_client = storage.Client()
+    storage_client = storage.Client(credentials=credentials)
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_blob_name)
 
